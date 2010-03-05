822611648f983f0c8545b7ea0d7de8465127010e
makoto_kw <makoto.kw@gmail.com>
2010-03-05 16:37:13 +0900

    Added new build scripts

---
+++ build/AssemblyProjectInfoTemplate.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("@COMPANY@")]
+[assembly: AssemblyProduct("@PRODUCT@")]
+[assembly: AssemblyCopyright("@COPYRIGHT@")]
+[assembly: AssemblyTrademark("@TRADEMARK@")]
+[assembly: AssemblyCulture("@CULTURE@")]
+[assembly: AssemblyVersionAttribute("@VERSION@")]
